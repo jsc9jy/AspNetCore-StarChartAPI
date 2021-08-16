68242387bb7c5be1cbf0115b035b80decf12c545
jsc9jy <31700320+jsc9jy@users.noreply.github.com>
2021-08-16 11:36:18 -0400

    Update CelestialObjectController.cs
    GetById, GetByName

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,50 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects
+                .FirstOrDefault(co => co.Id == id);
+
+            if (celestialObject == null)
+                return NotFound();
+
+            foreach (var cObject in _context.CelestialObjects)
+            {
+                if (cObject.OrbitedObjectId == id)
+                {
+                    celestialObject.Satellites.Add(cObject);
+                }
+            }
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects
+                .Where(celestialObject => celestialObject.Name == name)
+                .ToList();
+
+            if (celestialObjects.Count == 0)
+                return NotFound();
+
+            foreach (var cObject in _context.CelestialObjects)
+            {
+                var matchingCelestialObject = celestialObjects.FirstOrDefault(
+                    co => co.Id == cObject.OrbitedObjectId);
+
+                if (matchingCelestialObject != null)
+                {
+                    matchingCelestialObject.Satellites.Add(cObject);
+                }
+            }
+
+            return Ok(celestialObjects);
+        }
+
     }
 }
